26c2f8ff81c2021572948cd016bee6f3928d4d27
Piotr Kisiel <piotr.kisiel.6@gmail.com>
2025-06-11 10:58:06 +0200

    feat: fetch all codes and fetch random 8 countries

---
+++ src/components/CountriesList.vue
@@ -0,0 +1,43 @@
+<template>
+  <div>
+    <ul>
+      <li v-for="country in countries" :key="country.cca3">{{ country.name.common }}</li>
+    </ul>
+  </div>
+</template>
+
+<script setup>
+import { ref, onMounted } from 'vue';
+
+const codes = ref([]);
+const randomCodes = ref([]);
+const countries = ref([]);
+
+onMounted(async () => {
+  // Fetch country codes
+  const response = await fetch('https://restcountries.com/v3.1/all?fields=cca3');
+  const json = await response.json();
+  codes.value = json.map((obj) => obj.cca3);
+
+  // Get 8 random country codes
+  let i = 0;
+  while (i < 8) {
+    const randomIndex = Math.floor(Math.random() * codes.value.length);
+    if (!randomCodes.value.includes(codes.value[randomIndex])) {
+      randomCodes.value.push(codes.value[randomIndex]);
+      i++;
+    }
+  }
+
+  // Create a string of country codes for the API call
+  const codesString = randomCodes.value.join(',');
+
+  // Fetch country data for the random codes
+  const countriesResponse = await fetch(
+    `https://restcountries.com/v3.1/alpha?codes=${codesString}`,
+  );
+  countries.value = await countriesResponse.json();
+});
+</script>
+
+<style lang="scss" scoped></style>
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,7 +1,15 @@
-<script setup></script>
+<script setup>
+import FilterBar from '@/components/FilterBar.vue'
+import CountriesList from '@/components/CountriesList.vue'
+</script>
 
 <template>
-  <main>
-    <h2>Home</h2>
-  </main>
+  <div class="content-wrapper">
+    <div class="filter-wrapper">
+      <FilterBar />
+    </div>
+    <main class="main-wrapper">
+      <CountriesList />
+    </main>
+  </div>
 </template>
